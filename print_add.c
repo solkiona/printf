66ea2e9ca770a7829f38bce6142f611cd18bbf97
Solomon Adule <122953638+solkiona@users.noreply.github.com>
2023-04-24 18:14:48 +0100

    Update print_add.c

--- print_add.c
+++ print_add.c
@@ -1,26 +1,183 @@
 #include "main.h"
+/**
+ * prinlupx - prints a long decimal in hexadecimal
+ * @arguments: The character to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinlupx(va_list arguments, char *buf, unsigned int ibuf)
+{
+	long int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_input = va_arg(arguments, long int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = fill_hex_array(binary, hexadecimal, 1, 16);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
 
 /**
- * print_add - prints an address specifier
- * @list: input address
- * Return: Number of printed characters
+ * prinnhex - print number in hex begining with zero
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
  */
-int print_add(va_list list)
+int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
 {
-	void *add;
-	int len;
+	int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
 
-	add = va_arg(list, void *);
-
-	if (!add)
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
 	{
+		ibuf = handl_buf(buf, '0', ibuf);
 		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	ibuf = handl_buf(buf, '0', ibuf);
+	ibuf = handl_buf(buf, 'x', ibuf);
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	hexadecimal = malloc(sizeof(char) * (8 + 1));
+	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 8);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	{
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count + 2);
+}
+
+/**
+ * prinnoct - print the number in octal begining with zero
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinnoct(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input, i, isnegative, count, first_digit;
+	char *octal, *binary;
+
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	ibuf = handl_buf(buf, '0', ibuf);
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	octal = malloc(sizeof(char) * (11 + 1));
+	octal = fill_oct_array(binary, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, octal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(octal);
+	return (count + 1);
+}
+/**
+ * prinpint - print integer with plus symbol
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinpint(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input;
+	unsigned int int_in, int_temp, i, div;
+
+	int_input = va_arg(arguments, int);
+	if (int_input < 0)
+	{
+		int_in = int_input * -1;
+		ibuf = handl_buf(buf, '-', ibuf);
 	}
 	else
 	{
-		len += _write_char('0');
-		len += _write_char('x');
-		len += print_hex(list);
-		return (len);
+		int_in = int_input;
+		ibuf = handl_buf(buf, '+', ibuf);
 	}
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i + 1);
+}
+
+/**
+ * print_prg - writes the character c to stdout
+ * @a: input char
+ * @buf: buffer pointer
+ * @i: index for buffer pointer
+ * Return: On success 1.
+ */
+int print_prg(va_list a __attribute__((unused)), char *buf, unsigned int i)
+{
+	handl_buf(buf, '%', i);
+
+	return (1);
 }
